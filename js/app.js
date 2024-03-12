4861ed7b612ce93dcfada8fb378f411a1b55a206
giannisaladino <saladinogianni6@gmail.com>
2024-03-12 16:57:57 +0100

    EDIT: finished

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // 1 CHIEDIAMO ALL'UTENTE DI INSERIRE UNA PAROLA
 
-// const userWord = prompt('Inserisci una parola');
+const userWord = prompt('Inserisci una parola');
 // console.log(userWord);
 
 // 2 Creare una funzione per capire se la parola inserita è palindroma:
@@ -13,17 +13,36 @@
 function isPalindrome(word) {
     console.log(word);
 
+    let inverseWord = ''; //string
+
     for(let index = word.length - 1; index >= 0; index--) {
-        const inverseWord = word[index];
-        console.log(inverseWord);
+        const character = word[index];
+        // console.log(character);
+
+        inverseWord += character;
+        // inverseWord = inverseWord + character        
         
     }
 
-    return false;
+    // console.log(inverseWord === word);
+
+    if (inverseWord === word) {
+        console.log('La parola è palindroma!');
+        return true;
+    } else {
+        console.log('La parola non è palindroma :(');
+        return false;
+    }
+
+    // const result = inverseWord === word;
+    // return result;
+
+    // return inverseWord === word;
 }
 
+
 // invoco la funzione per la verifica
- const esito = isPalindrome('anna');
+ const esito = isPalindrome(userWord);
  console.log(esito);
 
 // alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
